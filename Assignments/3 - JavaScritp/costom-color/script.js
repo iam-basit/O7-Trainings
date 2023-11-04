36ce316579560dbb3c9b545ad5773b2b1b00300d
Bsit Malik <basithamed246@gmail.com>
2023-11-04 18:58:20 +0530

    New files are added

---
+++ Assignments/3 - JavaScritp/costom-color/script.js
@@ -0,0 +1,4 @@
+function change() {
+  value = document.getElementById('color').value
+  document.getElementById('change-color').style.backgroundColor = value
+}
---
+++ Assignments/3 - JavaScritp/mouseover/script.js
@@ -0,0 +1,16 @@
+function hover2() {
+  document.getElementById('img-1').src = 'img-2.jpg'
+}
+function out() {
+  document.getElementById('img-1').src = 'img-1.jpg'
+}
+
+function hover3() {
+  document.getElementById('img-1').src = 'img-3.jpg'
+}
+function hover4() {
+  document.getElementById('img-1').src = 'img-4.jpg'
+}
+function hover5() {
+  document.getElementById('img-1').src = 'img-5.jpg'
+}
