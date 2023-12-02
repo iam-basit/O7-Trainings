49884746aa2fd108f8b1e7a06e2808b5aa10cc28
Bsit Malik <basithamed246@gmail.com>
2023-12-03 03:41:15 +0530

    files modified!

--- 4-Advanced-JS/tests/test-2/script.js
+++ 4-Advanced-JS/tests/test-2/script.js
@@ -1,14 +1,19 @@
 var namesList = []
+
 function addBtn() {
   var names = document.getElementById('names').value
   namesList.push(names)
+  console.log(namesList)
 }
 
 function showBtn() {
   var enterdNames = document.getElementById('enterdNames')
-  var newAddedOptions = document.createElement('option')
 
-  newAddedOptions.innerHTML = enterdNames
-  enterdNames.appendChild(newAddedOptions)
+  for (var i = 0; i < namesList.length; i++) {
+    var newAddedOptions = document.createElement('option')
+    newAddedOptions.innerText = namesList[i]
+    enterdNames.appendChild(newAddedOptions)
+  }
+
   console.log(namesList)
 }
