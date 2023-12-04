01ee609b2213ddb8fa72cdc95ee4b49ee0c15023
Bsit Malik <basithamed246@gmail.com>
2023-12-04 13:01:40 +0530

    Map Class Work!

---
+++ class work/map/src/App.js
@@ -0,0 +1,30 @@
+import React, { Fragment } from 'react';
+import './App.css';
+import Child from './Child';
+function App() {
+  var friends=["nancy","jasleen","anikita","mridul","prabhjot","basit"]
+  return (
+    <Fragment>
+        <h1>Hello friends</h1>
+        {friends.map(
+          (el,index,array)=>{
+            return(
+              <React.Fragment key={index}>
+                <h1>{el} {index+1}</h1>
+                <p>This is para</p>
+                {array}
+              </React.Fragment>
+            )
+          }
+        )}
+{/* 
+  <React.Fragment></React.Fragment> (import React from "react")
+  <Fragment></Fragment> (import {Fragment} from "react")
+  <></> 
+*/}
+    </Fragment>
+    
+  );
+}
+
+export default App;
