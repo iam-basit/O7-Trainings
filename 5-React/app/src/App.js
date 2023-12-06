9f03cd9184c2199c24eba4b7cd1715286a105775
Bsit Malik <basithamed246@gmail.com>
2023-12-06 12:29:00 +0530

    creating login and register form using useNavigate hook!

--- 5-React/app/src/App.js
+++ 5-React/app/src/App.js
@@ -1,47 +1,16 @@
 import './App.css'
 import React, { Fragment } from 'react'
-import { useState } from 'react'
+// import { useState } from 'react'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
 function App() {
-  let [email, setEmail] = useState('')
-  let [password, setPassword] = useState('')
-
-  const passData = () => {
-    alert(
-      `
-      Email:  ${email}
-      Password:  ${password}
-      `,
-    )
-  }
-
   return (
     <>
-      <h1>Login form</h1>
-      <form onSubmit={passData}>
-        Email:
-        <input
-          type="email"
-          id="email"
-          name="email"
-          onChange={(fun) => {
-            setEmail(fun.target.value)
-          }}
-        />
-        <br />
-        Password:
-        <input
-          type="Password"
-          id="pass"
-          name="pass"
-          onChange={(fun) => {
-            setPassword(fun.target.value)
-          }}
-        />
-        <br />
-        <br />
-        <input type="submit" />
-      </form>
+      <BrowserRouter>
+        <Routes>
+          <Route path="" element={<loginForm/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </>
   )
 }
---
+++ 5-React/app/src/login-useNavigate.js
@@ -0,0 +1,42 @@
+export default function loginForm() {
+
+    let [email, setEmail] = useState('')
+  let [password, setPassword] = useState('')
+
+  const passData = () => {
+    alert(
+      `
+      Email:  ${email}
+      Password:  ${password}
+      `,
+    )
+  return (
+    <>
+      <h1>Login form</h1>
+      <form onSubmit={passData}>
+        Email:
+        <input
+          type="email"
+          id="email"
+          name="email"
+          onChange={(fun) => {
+            setEmail(fun.target.value)
+          }}
+        />
+        <br />
+        Password:
+        <input
+          type="Password"
+          id="pass"
+          name="pass"
+          onChange={(fun) => {
+            setPassword(fun.target.value)
+          }}
+        />
+        <br />
+        <br />
+        <input type="submit" />
+      </form>
+    </>
+  )
+}
