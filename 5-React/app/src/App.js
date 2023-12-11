a7c2fd8ad01b75ae1c2ab7e0cc4769c19deaf56e
Bsit Malik <basithamed246@gmail.com>
2023-12-11 22:14:18 +0530

    Assignment of bootstrap to React!

--- 5-React/app/src/App.js
+++ 5-React/app/src/App.js
@@ -1,201 +1,318 @@
 import React from 'react'
 import './App.css'
-// import CounterBtn from './CounterBtn'
 
 function App() {
   return (
     <>
-      <div class="fox-header">
-        <div class="inner-div">
-          <h1>FOX.</h1>
-          <h4>University</h4>
-
-          <div class="email-portion">
-            <i class="fa-sharp fa-solid fa-paper-plane fa-beat  email-icon"></i>
-            <span class="email-text">Email</span>
-            <span class="your-email">youremail@email.com</span>
-          </div>
-
-          <div class="call-portion">
-            <i class="fa-solid fa-phone fa-shake call-icon"></i>
-            <span class="call-text">Call</span>
-            <span class="call-us">Call Us:+1234567890</span>
-          </div>
-
-          <button type="button" class="apply-btn">
-            Apply now
-          </button>
+      <div className="container-fluid">
+        {/* <div class="card" style="width: 100%;">
+        <div class="card-header">
+            <div class="row">
+                <div class="col-md-4  ">
+                    <div class="row">
+                        <div class="col-md-2"><i class="fa-solid fs-3 fa-bars p-2"></i></div>
+                        <div class="col-md-4 fs-4">Home</div>
+                        <div class="col-md-6 fs-4">Contact</div>
+                    </div>
+                </div>
+                <div class="col-md-8">
+                    <div class="row">
+                       <div class="col-md-9"></div> 
+                        <div class=" pt-2 offset-md-9 col-md-3">
+                            <div class="row">
+                                <div class="col-md-3"><i class="fa-solid fa-magnifying-glass"></i></div>
+                                <div class="col-md-3">
+                                    <i  class=" fa-solid fa-message position-relative">
+                                        <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                          3
+                                        </span>
+                                    </i>
+                                </div>
+                                <div class="col-md-3"> 
+                                    <i  class=" fa-regular fa-bell position-relative">
+                                    <span class="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                                      3
+                                    </span>
+                                </i>
+                            </div>
+                                <div class="col-md-3">
+                                    <i class="fa-solid fa-maximize"></i>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                        
+                </div>
+            </div> 
         </div>
-      </div>
-
-      <div class="navbar">
-        <div class="nav-inner-div">
-          <ul>
-            <li>
-              <a href="#">Home</a>
-            </li>
-            <li>
-              <a href="#">About</a>
-            </li>
-            <li>
-              <a href="#">Courses</a>
-            </li>
-            <li>
-              <a href="#">Staff</a>
-            </li>
-            <li>
-              <a href="#">Blog</a>
-            </li>
-            <li>
-              <a href="#">Contact</a>
-            </li>
-          </ul>
-        </div>
-
-        <input type="search" id="search-bar" placeholder="Search" />
-        <i class="fa-solid fa-magnifying-glass search-icon"></i>
-      </div>
-
-      <div class="background-image">
-        <div class="color-over-img">
-          <div class="bgi-text">
-            <h1>
-              University, College <br /> School Education
-            </h1>
-            <p>
-              A small river named Dude flows by their place and supplies it with{' '}
-              <br /> the necessary regelialia.
-            </p>
-
-            <button class="bgi-btn">Contact Us</button>
-          </div>
-        </div>
-      </div>
-
-      <div class="cards">
-        <div class="teachers-content content">
-          <i class="fa-solid fa-chalkboard-user"></i>
-          <h4>Citified Teachers</h4>
-          <p>
-            Even the all-powerful pointing has no control about the blind texts
-            it is an almost unortographic.
-          </p>
-        </div>
-
-        <div class="education-content content">
-          <i class="fa-solid fa-book-open-reader"></i>
-          <h4>Special Education</h4>
-          <p>
-            Even the all-powerful pointing has no control about the blind texts
-            it is an almost unortographic.
-          </p>
-        </div>
-
-        <div class="libr/ary-content content">
-          <i class="fa-solid fa-book"></i>
-          <h4>Books & Libr/ary</h4>
-          <p>
-            Even the all-powerful pointing has no control about the blind texts
-            it is an almost unortographic.
-          </p>
-        </div>
-        <div class="sport-content content">
-          <i class="fa-solid fa-medal"></i>
-          <h4>Sport Club</h4>
-          <p>
-            Even the all-powerful pointing has no control about the blind texts
-            it is an almost unortographic.
-          </p>
-        </div>
-      </div>
-
-      <div class="offer-portion">
-        <div class="offer-header">
-          <h1>What We Offer</h1>
-          <p>
-            On her we she met a copy. The copy warned the Little Blind Text,
-            That where it come from it would have been rewritten a thousand
-            times and everything that was left from its origin would be the
-            world.
-          </p>
-        </div>
-        <div class="offers">
-          <div class="row">
-            <div class="first-col">
-              <div class="icon-div">
-                <i class="fa-solid fa-shield"></i>
+    </div> */}
+        <div className="row">
+          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+            <div className="container-fluid">
+              <i className="fa-solid fs-3 fa-bars p-2" />
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div
+                className="collapse navbar-collapse"
+                id="navbarSupportedContent"
+              >
+                <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li className="nav-item">
+                    <a className="nav-link active" aria-current="page" href="#">
+                      Home
+                    </a>
+                  </li>
+                  <li className="nav-item">
+                    <a className="nav-link" href="#">
+                      Contact
+                    </a>
+                  </li>
+                </ul>
+                <div className="nav-item">
+                  <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li className="nav-item p-3">
+                      <i className="fa-solid fa-magnifying-glass" />
+                    </li>
+                    <li className="p-3">
+                      <i className=" fa fa-solid fa-message position-relative">
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                          3
+                        </span>
+                      </i>
+                    </li>
+                    <li className="p-3">
+                      <i className=" fa-regular fa-bell position-relative">
+                        <span className="position-absolute top-0 start-100 translate-middle badge rounded-pill bg-danger">
+                          3
+                        </span>
+                      </i>
+                    </li>
+                    <li className="p-3">
+                      <i className="fa-solid fa-maximize" />
+                    </li>
+                  </ul>
+                </div>
               </div>
-              <h4>Safety First</h4>
-              <p>
-                Your time is limited, so don't waste it living someone else's
-                life. Don't be trapped by dogma
-                <br />
-                -Steve Jobs
-              </p>
             </div>
-            <div class="second-col">
-              <div class="icon-div">
-                <i class="fa-solid fa-book-open-reader"></i>
+          </nav>
+        </div>
+        <div className="row">
+          <div className="col-md-3 fs-1">Dashboard</div>
+          <div className="offset-md-7 col-md-2 fs-5 pt-2">Home/ Dashboard</div>
+        </div>
+        <div className="row mt-3">
+          <div className="col-md-3">
+            <div className="card bg-info ">
+              <div className="card-body ">
+                <div className="row">
+                  <div className="col-md-8 text-light">
+                    <h1 className="card-title">150</h1>
+                    <p className="card-text">New Orders</p>
+                  </div>
+                  <div className="col-md-4 mt-4">
+                    <i className="fa-solid fa-2x fa-bag-shopping opacity-50" />
+                  </div>
+                </div>
               </div>
-              <h4>Regular Classes</h4>
-              <p>
-                The greatest glory in living lies not in never falling, but in
-                rising every time we fall. <br />
-                -Nelson Mandela
-              </p>
+              <div className="card-footer">
+                <h6 className="text-center text-light">
+                  More info{' '}
+                  <span>
+                    <i className="fa-solid fa-arrow-right" />
+                  </span>
+                </h6>
+              </div>
             </div>
           </div>
-
-          <div class="row">
-            <div class="first-col">
-              <div class="icon-div">
-                <i class="fa-solid fa-chalkboard-user"></i>
+          <div className="col-md-3">
+            <div className="card bg-success ">
+              <div className="card-body ">
+                <div className="row">
+                  <div className="col-md-8 text-light">
+                    <h1 className="card-title">53%</h1>
+                    <p className="card-text">Bounce Rate</p>
+                  </div>
+                  <div className="col-md-4 mt-4">
+                    <i className="fa-solid fa-2x opacity-50 fa-chart-simple " />
+                  </div>
+                </div>
               </div>
-              <h4>Certified Teachers</h4>
-              <p>
-                If you look at what you have in life, you'll always have more.
-                If you look at what you don't have in life, you'll never have
-                enough. <br /> -Oprah Winfrey
-              </p>
-            </div>
-            <div class="second-col">
-              <div class="icon-div">
-                <i class="fa-solid fa-people-roof"></i>
+              <div className="card-footer">
+                <h6 className="text-center text-light">
+                  More info{' '}
+                  <span>
+                    <i className="fa-solid fa-arrow-right" />
+                  </span>
+                </h6>
               </div>
-              <h4>Sufficient Classrooms</h4>
-              <p>
-                If you set your goals ridiculously high and it's a failure, you
-                will fail above everyone else's success. <br /> -James Cameron
-              </p>
             </div>
           </div>
-
-          <div class="row">
-            <div class="first-col">
-              <div class="icon-div">
-                <i class="fa-brands fa-slack"></i>
+          <div className="col-md-3">
+            <div className="card bg-warning">
+              <div className="card-body ">
+                <div className="row">
+                  <div className="col-md-8">
+                    <h1 className="card-title">44</h1>
+                    <p className="card-text">User Registrations</p>
+                  </div>
+                  <div className="col-md-4 mt-4">
+                    <i className="fa-solid fa-user-plus fa-2x opacity-50" />
+                  </div>
+                </div>
               </div>
-              <h4>Creative Lessons</h4>
-              <p>
-                Spread love everywhere you go. Let no one ever come to you
-                without leaving happier.
-                <br /> -Mother Teresa
-              </p>
+              <div className="card-footer">
+                <h6 className="text-center text-light">
+                  More info{' '}
+                  <span>
+                    <i className="fa-solid fa-arrow-right" />
+                  </span>
+                </h6>
+              </div>
             </div>
-            <div class="second-col">
-              <div class="icon-div">
-                <i class="fa-regular fa-futbol"></i>
+          </div>
+          <div className="col-md-3">
+            <div className="card bg-danger">
+              <div className="card-body ">
+                <div className="row">
+                  <div className="col-md-8 text-light">
+                    <h1 className="card-title">65</h1>
+                    <p className="card-text">Unique Visitors</p>
+                  </div>
+                  <div className="col-md-4 mt-4">
+                    <i className="fa-solid fa-chart-pie opacity-50 fa-2x" />
+                  </div>
+                </div>
               </div>
-              <h4>Sports Facilities</h4>
-              <p>
-                You may say I'm a dreamer, but I'm not the only one. I hope
-                someday you'll join us. And the world will live as one. <br />{' '}
-                -John Lennon
-              </p>
+              <div className="card-footer">
+                <h6 className="text-center text-light">
+                  More info{' '}
+                  <span>
+                    <i className="fa-solid fa-arrow-right" />
+                  </span>
+                </h6>
+              </div>
             </div>
           </div>
         </div>
-        <div class="side-bar"></div>
+        <div className="row mt-3 mb-4">
+          <div className="col-md-7">
+            <div className="card">
+              <div className="card-header">
+                <div className="row">
+                  <div className="col-md-2 mt-2">
+                    <i className="fa-solid  fs-4 fa-chart-pie" />
+                    <span className="fs-4">Sales</span>
+                  </div>
+                  <div className=" offset-md-7 mt-2 col-md-3">
+                    <button className="btn btn-primary">Area</button>
+                    <span className="ms-3">Donut</span>
+                  </div>
+                </div>
+              </div>
+              <div className="card-body">
+                <div className="row">
+                  <img src="task4.png" alt="" />
+                </div>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-5">
+            <div className="card bg-primary">
+              <div className="card-body">
+                <div className="row">
+                  <div className="col-md-4 text-light">
+                    <div className="row">
+                      <div className="col-md-2">
+                        <i className="text-light fa-solid fa-location-dot fa-2x" />
+                      </div>
+                      <div className=" ms-2 col-md-9">
+                        <span className="fs-5">Visitors</span>
+                      </div>
+                    </div>
+                  </div>
+                  <div className=" offset-md-6 col-md-2 text-light">
+                    <div className="row">
+                      <div className="col-md-6">
+                        <i className="fa-solid fa-calendar-days" />
+                      </div>
+                      <div className="col-md-6">
+                        <i className="fa-solid fa-ellipsis" />
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="row">
+                  <div className="col-md-12 mt-4">
+                    <img
+                      src="World-Map-PNG-Photos.png"
+                      className="card-image w-100"
+                      alt=""
+                    />
+                  </div>
+                </div>
+                <div className="row mt-3">
+                  <div className="col-md-4">
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <img
+                          src="line-graph.png"
+                          className="card-image w-100"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <h5 className="text-light text-center">Visitors</h5>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="col-md-4">
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <img
+                          src="line-graph.png"
+                          className="card-image w-100"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <h5 className="text-light text-center">Online</h5>
+                      </div>
+                    </div>
+                  </div>{' '}
+                  <div className="col-md-4">
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <img
+                          src="line-graph.png"
+                          className="card-image w-100"
+                          alt=""
+                        />
+                      </div>
+                    </div>
+                    <div className="row mt-2">
+                      <div className="offset-md-4 col-md-8">
+                        <h5 className="text-light text-center">Sales</h5>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   )
