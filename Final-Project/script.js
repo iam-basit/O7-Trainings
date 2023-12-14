619f9cad886db43b761cc6f2e7c12c1d9d5410e0
Bsit Malik <basithamed246@gmail.com>
2023-12-14 20:07:27 +0530

    Working on bannar part!

--- Final-Project/script.js
+++ Final-Project/script.js
@@ -1,18 +1,16 @@
 function shopClicked() {
   var text = document.getElementById('content')
-  text.style.display = text.style.display == 'none' ? 'block' : 'none'
-
+  if (text.classList.contains('block')) {
+    text.classList.remove('block')
+    text.classList.add('none')
+  } else {
+    text.classList.add('block')
+    text.classList.remove('none')
+  }
   // Add event listener to close the dropdown when clicking outside of it
   document.addEventListener('click', function (event) {
     var dropdownButton = document.getElementById('dropdown-button')
     var dropdownContent = document.getElementById('content')
-
-    // Check if the clicked element is not the dropdown button or content
-    if (
-      event.target !== dropdownButton &&
-      !dropdownContent.contains(event.target)
-    ) {
-      dropdownContent.style.display = 'none'
-    }
+    var text = document.getElementById('content')
   })
 }
